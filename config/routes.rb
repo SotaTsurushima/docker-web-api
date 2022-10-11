2bdba92adcb869f0e0b76e8c9c20908d8469e5a1
SotaTsurushima <2018cassotatsurushima@gmail.com>
2022-10-12 00:12:39 +0900

    made spotify artist searach function

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,4 @@ Rails.application.routes.draw do
   root to: 'top#index'
   get '/goods/search' , to: 'goods#search'
   get '/musics/search' , to: 'musics#search'
-  resources :musics do
-    collection {get "search"}
-  end
 end
