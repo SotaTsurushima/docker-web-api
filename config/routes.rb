2be9e416b526dfe52bd4ef66574b010dbee148cb
SotaTsurushima <2018cassotatsurushima@gmail.com>
2022-10-11 23:40:10 +0900

    Spotify ページに遷移できるようlink_to作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root to: 'top#index'
   get '/goods/search' , to: 'goods#search'
+  get '/musics/search' , to: 'musics#search'
+  resources :musics do
+    collection {get "search"}
+  end
 end
