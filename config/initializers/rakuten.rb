34f80febf46adf45c79d23773f17c1c127af2daa
T$uru$hima <67181317+SotaTsurushima@users.noreply.github.com>
2022-10-16 20:32:50 +0900

    rakuten.rbへのapplication_id, affiliation_idの直打ちを辞めて.envへ移動

--- config/initializers/rakuten.rb
+++ config/initializers/rakuten.rb
@@ -1,4 +1,4 @@
 RakutenWebService.configure do |c|
-    c.application_id = '1042959236745055293'
-    c.affiliate_id = '2ae13a74.ea4d7122.2ae13a75.4f68cf72'
+    c.application_id = ENV['RWS_APPLICATION_ID']
+    c.affiliate_id = ENV['RWS_AFFILIATION_ID']
 end
