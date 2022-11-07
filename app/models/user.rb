624f9a361d3d877522a7bed6a1a9acefcd23a871
T$uru$hima <67181317+SotaTsurushima@users.noreply.github.com>
2022-11-07 12:33:27 +0900

    name, picture urlが入るよう処理を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,23 @@ class User < ApplicationRecord
     response = Net::HTTP.get_response(URI.parse(uri)).body
     res = JSON.parse(response)
 
-    hash_name = []
-    hash_name.push(res["results"][0]["name"]["title"])
-    hash_name.push(res["results"][0]["name"]["first"])
-    hash_name.push(res["results"][0]["name"]["last"])
-    name = hash_name.join(' ')
+    user_data = []
+    user_name = []
     
+    # user_name作成
+    user_name.push(res["results"][0]["name"]["title"])
+    user_name.push(res["results"][0]["name"]["first"])
+    user_name.push(res["results"][0]["name"]["last"])
+    user_name = user_name.join(' ')
+    user_data.push(user_name)
+
+    # 写真URLの作成
+    user_data.push(res["results"][0]["picture"]["medium"])
+
+
+    binding.pry
+    # 写真URLの追加
+
     return name
   end
 end
