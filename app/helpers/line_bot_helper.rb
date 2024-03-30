a4b0bd3f8d73a2faeffffeae4a359e7a08178623
SotaTsurushima <2018cassotatsurushima@gmail.com>
2024-03-30 22:07:34 +0900

    added helper for line

--- app/helpers/line_bot_helper.rb
+++ app/helpers/line_bot_helper.rb
@@ -1,2 +1,19 @@
 module LineBotHelper
+  def raku_search(received_text)
+    # t['mediumImageUrls'](存在すれば)
+    # 20件取得する
+    rakuten = RakutenWebService::Ichiba::Item.search(keyword: received_text)
+    rakuten_items = []
+    
+    for t in rakuten
+      rakuten_items.push(
+        t['mediumImageUrls'],
+        t['itemName'],
+        t['itemPrice'],
+        t['reviewAverage']
+      )
+    end
+
+    return rakuten_items
+  end
 end
