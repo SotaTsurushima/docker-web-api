94ff5edad610083347935a1db99b7d2dd270e73c
SotaTsurushima <2018cassotatsurushima@gmail.com>
2024-04-09 23:20:11 +0900

    add function

--- app/helpers/line_bot_helper.rb
+++ app/helpers/line_bot_helper.rb
@@ -1,23 +1,27 @@
 module LineBotHelper
   def raku_search(received_text)
-    # t['mediumImageUrls'](存在すれば)
-    # 20件取得する
     rakuten_response = RakutenWebService::Ichiba::Item.search(keyword: received_text)
     rakuten_items = []
     
-    rakuten_response.each do |t|
-      if t['reviewAverage'] > 4.5 && rakuten_items.count < 5
+    rakuten_response.each do |r|
+      if r['reviewAverage'] > 4.5 && rakuten_items.count < 5
         rakuten_items.push(
           {
-            mediumImageUrls: t['mediumImageUrls'],
-            itemName: t['itemName'],
-            itemPrice: t['itemPrice'],
-            reviewAverage: t['reviewAverage']
+            mediumImageUrls: r['mediumImageUrls'],
+            itemName: filter_itemName(received_text, r),
+            itemPrice: r['itemPrice'],
+            reviewAverage: r['reviewAverage'],
+            itemUrl: r['itemUrl']
           }
         )
       end
     end
 
     return rakuten_items
+  end
+
+  def filter_itemName(text, rakuten_item)
+    itemName = rakuten_item['itemName'].slice(0, 15)
+    return itemName
   end
 end
