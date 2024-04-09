a5dc695a9ae9bbe8d335daa689a352cfc378a767
SotaTsurushima <2018cassotatsurushima@gmail.com>
2024-04-09 22:10:34 +0900

    add function para filter

--- app/helpers/line_bot_helper.rb
+++ app/helpers/line_bot_helper.rb
@@ -2,16 +2,20 @@ module LineBotHelper
   def raku_search(received_text)
     # t['mediumImageUrls'](存在すれば)
     # 20件取得する
-    rakuten = RakutenWebService::Ichiba::Item.search(keyword: received_text)
+    rakuten_response = RakutenWebService::Ichiba::Item.search(keyword: received_text)
     rakuten_items = []
     
-    for t in rakuten
-      rakuten_items.push(
-        t['mediumImageUrls'],
-        t['itemName'],
-        t['itemPrice'],
-        t['reviewAverage']
-      )
+    rakuten_response.each do |t|
+      if t['reviewAverage'] > 4.5 && rakuten_items.count < 5
+        rakuten_items.push(
+          {
+            mediumImageUrls: t['mediumImageUrls'],
+            itemName: t['itemName'],
+            itemPrice: t['itemPrice'],
+            reviewAverage: t['reviewAverage']
+          }
+        )
+      end
     end
 
     return rakuten_items
